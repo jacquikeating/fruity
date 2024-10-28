d9a4ca3b90f81ad78333cc8ab6d8c0dab96cbaed
jacquikeating <j.keating@live.ca>
2024-10-28 19:16:51 -0400

    ReportPage initial styling
    - Deleted preexisting styles from index.scss.
    - Added the first few global styles: margin reset and width & margins on <main>.
    - Added basic styling to ReportPage for readability.
    - Added a <div>, pull-nums, to contain both numToday and numTotal on a single line.
    - Wrapped the Twitch & FFLogs links inside the <p> tag that was previously a sibling.

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -6,21 +6,26 @@ const ReportPage = () => {
   return (
     <main className="report">
       <h1 className="report__heading">Report: {session1.sessionDate}</h1>
-      <p className="report__subtitle">Session {session1.sessionNum}</p>
-      <a className="report__link" href={session1.fflogsLink}>
-        Logs
-      </a>
-      <a className="report__link" href={session1.twitchLink}>
-        VoD
-      </a>
+      <p className="report__subtitle">
+        Session {session1.sessionNum}
+        <a className="report__link" href={session1.fflogsLink}>
+          Logs
+        </a>
+        <a className="report__link" href={session1.twitchLink}>
+          VoD
+        </a>
+      </p>
 
       <h2 className="report__subheading">Pulls ({session1.pulls.length})</h2>
       <ul className="report__pulls-list">
         {session1.pulls.map((pull) => {
           return (
             <li className="pull" key={pull.pullNumTotal}>
-              <h3 className="pull__num">Pull {pull.pullNumToday}</h3>
-              <p className="pull__numTotal">{pull.pullNumTotal}</p>
+              <div className="pull__nums">
+                <h3 className="pull__num-today">Pull {pull.pullNumToday}</h3>
+                <p className="pull__num-total">{pull.pullNumTotal} Overall</p>
+              </div>
+
               <p className="pull__info">
                 Phase {pull.phase}, {pull.mech}
               </p>
