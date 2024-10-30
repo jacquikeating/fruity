3a5db52dfc83670592f5cd0850623ca80a7f79d5
jacquikeating <j.keating@live.ca>
2024-10-30 17:24:18 -0400

    Moved phase breakdown related code from page level to component level
    - Cut and pasted getPhaseBreakdown(), phaseBreakdown, and phasesArray from ReportPage into the PhaseBreakdownTable component.
    - Imported PhaseBreakdownTable into ReportPage.
    - Added a prop to allow the page to pass the session data down to the component.

--- src/components/PhaseBreakdownTable/PhaseBreakdownTable.jsx
+++ src/components/PhaseBreakdownTable/PhaseBreakdownTable.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./PhaseBreakdownTable.scss";
 
-const PhaseBreakdownTable = () => {
-  return <div>PhaseBreakdownTable</div>;
+const PhaseBreakdownTable = ({ sessionData }) => {
+  function getPhaseBreakdown() {
+    let phasesReached = [];
+
+    sessionData.pulls.map((pull) => {
+      phasesReached.push(pull.phase);
+    });
+
+    let phaseBreakdown = phasesReached.reduce((accumulator, phase) => {
+      return (
+        accumulator[phase] ? ++accumulator[phase] : (accumulator[phase] = 1),
+        accumulator
+      );
+    }, {});
+
+    return phaseBreakdown;
+  }
+
+  const phaseBreakdown = getPhaseBreakdown();
+
+  let phasesArray = [
+    phaseBreakdown[1] ? phaseBreakdown[1] : 0,
+    phaseBreakdown[2] ? phaseBreakdown[2] : 0,
+    phaseBreakdown[3] ? phaseBreakdown[3] : 0,
+    phaseBreakdown[4] ? phaseBreakdown[4] : 0,
+    phaseBreakdown[5] ? phaseBreakdown[5] : 0,
+    phaseBreakdown[6] ? phaseBreakdown[6] : 0,
+    phaseBreakdown[7] ? phaseBreakdown[7] : 0,
+  ];
+
+  return (
+  );
 };
 
 export default PhaseBreakdownTable;
