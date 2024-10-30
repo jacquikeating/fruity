f8b9fdb320cce49d3d494c1a74d3c38f6ea9bf75
jacquikeating <j.keating@live.ca>
2024-10-30 17:09:25 -0400

    Created PhaseBreakdownTable component
    - Created components folder as a child of src.
    - Created folder, JSX file, and SCSS file for PhaseBreakdownTable.

---
+++ src/components/PhaseBreakdownTable/PhaseBreakdownTable.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PhaseBreakdownTable.scss";
+
+const PhaseBreakdownTable = () => {
+  return <div>PhaseBreakdownTable</div>;
+};
+
+export default PhaseBreakdownTable;
