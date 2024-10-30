4f69e9cb781914aa410e2ac08dd15fcb42be378b
jacquikeating <j.keating@live.ca>
2024-10-30 17:29:14 -0400

    Built HTML <table>
    - Added a <table> element with two rows: first, the numbers of all phases; second, the number of times each phase was wiped on during a particular session.
    - Second row filled with values from phasesArray.

--- src/components/PhaseBreakdownTable/PhaseBreakdownTable.jsx
+++ src/components/PhaseBreakdownTable/PhaseBreakdownTable.jsx
@@ -32,6 +32,30 @@ const PhaseBreakdownTable = ({ sessionData }) => {
   ];
 
   return (
+    <table className="phases-table">
+      <tbody>
+        <tr className="phases-table__row">
+          <th className="phases-table__header">Phase</th>
+          <td className="phases-table__cell">1</td>
+          <td className="phases-table__cell">2</td>
+          <td className="phases-table__cell">3</td>
+          <td className="phases-table__cell">4</td>
+          <td className="phases-table__cell">5</td>
+          <td className="phases-table__cell">6</td>
+          <td className="phases-table__cell">7</td>
+        </tr>
+        <tr className="phases-table__row">
+          <th className="phases-table__header">Pulls</th>
+          <td className="phases-table__cell">{phasesArray[0]}</td>
+          <td className="phases-table__cell">{phasesArray[1]}</td>
+          <td className="phases-table__cell">{phasesArray[2]}</td>
+          <td className="phases-table__cell">{phasesArray[3]}</td>
+          <td className="phases-table__cell">{phasesArray[4]}</td>
+          <td className="phases-table__cell">{phasesArray[5]}</td>
+          <td className="phases-table__cell">{phasesArray[6]}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
