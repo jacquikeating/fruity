3ef9bb04afa48820d170cd8eb46b14ebaad3fa30
jacquikeating <j.keating@live.ca>
2024-10-29 21:26:04 -0400

    Created numSuffix() function
    - Wrote a function to append the correct suffix when converting numerals to ordinal numbers.
    - The text displaying the overall pull number now uses this function.

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -3,6 +3,20 @@ import { session1 } from "../../utils/old-data";
 import "./ReportPage.scss";
 
 const ReportPage = () => {
+  function numSuffix(number) {
+    const lastNumeral = JSON.stringify(number).slice(-1);
+    const last2Numerals = JSON.stringify(number).slice(-2); // console.log(last2Numerals);
+    if (lastNumeral == 1 && last2Numerals != 11) {
+      return "st";
+    } else if (lastNumeral == 2 && last2Numerals != 22) {
+      return "nd";
+    } else if (lastNumeral == 3 && last2Numerals != 33) {
+      return "rd";
+    } else {
+      return "th";
+    }
+  }
+
   return (
     <main className="report">
       <h1 className="report__heading">Report: {session1.sessionDate}</h1>
@@ -23,7 +37,10 @@ const ReportPage = () => {
             <li className="pull" key={pull.pullNumTotal}>
               <div className="pull__nums">
                 <h3 className="pull__num-today">Pull {pull.pullNumToday}</h3>
-                <p className="pull__num-total">{pull.pullNumTotal} Overall</p>
+                <p className="pull__num-total">
+                  {pull.pullNumTotal}
+                  {numSuffix(pull.pullNumTotal)} Overall
+                </p>
               </div>
 
               <p className="pull__info">
