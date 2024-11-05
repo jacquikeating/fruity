026e03385e885af19fa5e61367594ba3558315b8
jacquikeating <j.keating@live.ca>
2024-11-04 21:09:59 -0500

    Attemped fix for Heroku deploy
    - Attempting to fix Heroku error H20. Possibly related to port/env variables.
    - Added LOCAL_ADDRESS variable.

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import "dotenv/config";
 const app = express();
 
-let { PORT, CROSS_ORIGIN } = process.env;
-PORT = PORT || 8082;
+// let { PORT, CROSS_ORIGIN } = process.env;
+// PORT = PORT || 8082;
+const { PORT = 3000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
 
-app.use(cors({ origin: CROSS_ORIGIN }));
+// app.use(cors({ origin: CROSS_ORIGIN }));
 
 app.use(express.json());
 
@@ -14,6 +15,10 @@ app.get("/", (_req, res) => {
   res.send("If you're reading this, the server is running!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, LOCAL_ADDRESS, () => {
   console.log(`App running on port ${PORT}`);
 });
+// app.listen(PORT, LOCAL_ADDRESS, () => {
+//   const address = server.address();
+//   console.log("server listening at", address);
+// });
