e756ea6664c4238ee518426badb5222fcc6a4647
jacquikeating <j.keating@live.ca>
2024-11-08 19:36:59 -0500

    Fixed error in sessions migration
    - Erroneously used table.int when I needed table.integer.
    - Successfully ran first migrations!

--- migrations/20241109002050_create_sessions_table.js
+++ migrations/20241109002050_create_sessions_table.js
@@ -2,7 +2,7 @@ export const up = function (knex) {
   return knex.schema.createTable("session", (table) => {
     table.increments("id").primary();
     table.date("date").notNullable();
-    table.int("prog_phase").notNullable();
+    table.integer("prog_phase").notNullable();
     table.string("prog_mech").notNullable();
     table.string("fflogs_link");
     table.string("twitch_link");
