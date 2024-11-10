6d7eed6df031697050b94827a15d3d5c6c04a419
jacquikeating <j.keating@live.ca>
2024-11-10 18:38:57 -0500

    Added data from second session
    - Added to seed_data/pull.js with data from our second DSR session.
    - Added to seed_data/session.js with data from our second DSR session.

--- seed_data/session.js
+++ seed_data/session.js
@@ -5,6 +5,17 @@ export default [
     prog_mech: "Dives",
     fflogs_link: "",
     twitch_link: "",
+    roster: "Chro,Tori,Ruvien,Ella,Quil,Anivia,Char,Laveera",
+  },
+  {
+    date: "2023-08-09",
+    prog_phase: 4,
+    prog_mech: "Shockwaves",
+    fflogs_link: "",
+    twitch_link: "",
     roster: "Chro,Sophia,Ruvien,Ella,Quil,Hypatia,Char,Laveera",
   },
 ];
+
+// DSR Roster: "Chro,Tori,Ruvien,Ella,Quil,Anivia,Char,Laveera"
+// FRU Roster: "Chro,Sophia,Ruvien,Ella,Quil,Hypatia,Char,Laveera"
