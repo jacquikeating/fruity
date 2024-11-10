75be805bed806787498bd5aa467e2bc6bb4bf5a6
jacquikeating <j.keating@live.ca>
2024-11-10 13:56:37 -0500

    Adjusted pull table fields
    - Rearranged order of fields in pulls table in a way that makes more sense to me: prog_point_reached comes directly after phase and mech, instead of being separated from them by cause and players_responsible.
    - Added two more fields: log_link and clip_link. These will be used for specific pulls in which supplementary resources (a log and/or clip!) are provided.

--- migrations/20241109002620_create_pulls_table.js
+++ migrations/20241109002620_create_pulls_table.js
@@ -12,11 +12,13 @@ export const up = function (knex) {
     table.integer("pull_num_today").notNullable();
     table.string("phase").notNullable();
     table.string("mech").notNullable();
+    table.string("prog_point_reached");
     table.string("cause").notNullable();
     table.string("players_responsible");
     // Should it be responsible_player_1 and 2? references player name?
-    table.string("prog_point_reached");
     table.timestamp("ended_at").defaultTo(knex.fn.now());
+    table.string("log_link");
+    table.string("clip_link");
   });
 };
 
--- seed_data/pull.js
+++ seed_data/pull.js
@@ -1,11 +1,13 @@
 export default [
   {
-    session_id: 1,
+    session_id: 21,
     pull_num_today: 1,
     phase: 1,
     mech: "Playstation",
+    prog_point_reached: "old",
     cause: "Laveera chains",
     players_responsible: "Laveera",
-    prog_point_reached: "old",
+    log_link: "",
+    twitch_link: "",
   },
 ];
