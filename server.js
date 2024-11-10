ceb18d4c0e1cff03eccc5449706834b970cd4658
jacquikeating <j.keating@live.ca>
2024-11-10 18:09:16 -0500

    Added /pulls path
    - Imported pullRoutes into server.js.
    - Added app.use("/pulls", pullRoutes).
    - Tested endpoint and received expected response.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import sessionRoutes from "./routes/sessions.js";
+import pullRoutes from "./routes/pulls.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get("/", (_req, res) => {
 
 app.use(cors());
 app.use("/sessions", sessionRoutes);
+app.use("/pulls", pullRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`);
