6d901bd82d068c873542fac694a4677643f1bc2f
jacquikeating <j.keating@live.ca>
2024-11-10 23:35:04 -0500

    Added shared functions to shared-functions.js
    - Removed the following functions from their previous page/component-level locations:
       - createReadableDate()
       - numSuffix()
       - findStrugglePhase()
       - findStruggleMech()
       - findGoldStars()
       - getTextColour()

--- src/components/Pull/Pull.jsx
+++ src/components/Pull/Pull.jsx
@@ -1,21 +1,8 @@
 import React from "react";
+import { numSuffix } from "../../utils/shared-functions";
 import "./Pull.scss";
 
 const Pull = ({ pullData }) => {
-  function numSuffix(number) {
-    const lastNumeral = JSON.stringify(number).slice(-1);
-    const last2Numerals = JSON.stringify(number).slice(-2);
-    if (lastNumeral == 1 && last2Numerals != 11) {
-      return "st";
-    } else if (lastNumeral == 2 && last2Numerals != 12) {
-      return "nd";
-    } else if (lastNumeral == 3 && last2Numerals != 13) {
-      return "rd";
-    } else {
-      return "th";
-    }
-  }
-
   return (
     <li className="pull">
       <div className="pull__nums">
--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,78 +1,33 @@
 import React from "react";
 import { session1 } from "../../utils/old-data";
+import {
+  createReadableDate,
+  findGoldStars,
+  findStrugglePhase,
+  findStruggleMech,
+} from "../../utils/shared-functions.js";
+import axios from "axios";
 import PhaseBreakdownTable from "../../components/PhaseBreakdownTable/PhaseBreakdownTable";
 import Pull from "../../components/Pull/Pull.jsx";
 import "./ReportPage.scss";
 
 const ReportPage = () => {
-  function findGoldStars() {
-    let causedWipes = [];
-    let goldStars = [];
+  const [sessionData, setSessionData] = useState(null);
+  const { sessionID } = useParams();
 
-    session1.pulls.map((pull) => {
-      pull.playerNames.forEach((playerName) => {
-        if (!causedWipes.includes(playerName)) {
-          causedWipes.push(playerName);
-        }
-      });
-    });
-
-    session1.players.forEach((player) => {
-      if (!causedWipes.includes(player)) {
-        goldStars.push(player);
+  useEffect(() => {
+    async function getSessionData() {
+      try {
+        let result = await axios.get(`http://localhost:5050/sessions`);
+        let data = result.data;
+        setSessionData(data[0]);
+        createReadableDate(data[0].date);
+      } catch (error) {
+        console.error(error);
       }
-    });
-
-    if (goldStars.length === 0) {
-      return "None";
     }
-
-    return goldStars;
-  }
-
-  const findStrugglePhase = (arr) => {
-    const tallyObject = arr.reduce((accumulatedObject, thisPull) => {
-      accumulatedObject[thisPull.phase] =
-        (accumulatedObject[thisPull.phase] ?? 0) + 1;
-      return accumulatedObject;
-    }, {});
-
-    const tallyArray = Object.entries(tallyObject);
-
-    const highestCount = tallyArray.reduce(
-      (currentHighest, thisItem) => {
-        if (thisItem[1] >= currentHighest[1]) {
-          return thisItem;
-        } else {
-          return currentHighest;
-        }
-      },
-      [null, 0]
-    );
-    return `${highestCount[0]} (${highestCount[1]})`;
-  };
-
-  const findStruggleMech = (arr) => {
-    const tallyObject = arr.reduce((accumulatedObject, thisPull) => {
-      accumulatedObject[thisPull.mech] =
-        (accumulatedObject[thisPull.mech] ?? 0) + 1;
-      return accumulatedObject;
-    }, {});
-
-    const tallyArray = Object.entries(tallyObject);
-
-    const highestCount = tallyArray.reduce(
-      (currentHighest, thisItem) => {
-        if (thisItem[1] >= currentHighest[1]) {
-          return thisItem;
-        } else {
-          return currentHighest;
-        }
-      },
-      [null, 0]
-    );
-    return `${highestCount[0]} (${highestCount[1]})`;
-  };
+    getSessionData();
+  }, []);
 
   return (
     <main className="report">
@@ -102,14 +57,13 @@ const ReportPage = () => {
         </p>
         <p className="report__extra-info">
           <span className="report__extra-info--bold">Gold Stars:</span>{" "}
-          {findGoldStars()}
+          {findGoldStars(session1.pulls, session1.players)}
         </p>
       </section>
-
+      ) : ( "" )}
       <section className="report__section">
         <PhaseBreakdownTable sessionData={session1} />
       </section>
-
       <section className="report__section">
         <h2 className="report__subheading">Pulls ({session1.pulls.length})</h2>
         <ul className="report__pulls-list">
