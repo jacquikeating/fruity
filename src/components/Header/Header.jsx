17af482e47ea87be48de6f5a41457fd4b96822a9
jacquikeating <j.keating@live.ca>
2024-11-11 16:10:30 -0500

    Created Header component
    - Created folder and files for Header component.
    - Added in basic HTML structure: header, nav, and ul.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul></ul>
+      </nav>
+    </header>
+  );
+};
