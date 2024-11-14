a9494d76dac3e69551b0f7d5181e3ecff90f320a
jacquikeating <j.keating@live.ca>
2024-11-14 14:28:37 -0500

    Pulls list code put into SessionsList component
    - Removed all code that dynamically generated a list of <Pull> elements from ReportPage and its SCSS file.
    - Pasted into SessionsList component.
    - Modified for new context.

--- src/components/SessionsList/SessionsList.jsx
+++ src/components/SessionsList/SessionsList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./SessionsList.scss";
 
-const SessionsList = () => {
-  return <div>SessionsList</div>;
+const SessionsList = ({ sessionsArray }) => {
+  return (
+    <ul className="sessions-list">
+      {sessionsArray.map((session) => {
+        // return <Session key={session.id} sessionData={session} />;
+      })}
+    </ul>
+  );
 };
 
 export default SessionsList;
--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -8,7 +8,6 @@ import {
   findStruggleMech,
 } from "../../utils/shared-functions.js";
 import PhaseBreakdownTable from "../../components/PhaseBreakdownTable/PhaseBreakdownTable";
-import Pull from "../../components/Pull/Pull.jsx";
 import PullsTable from "../../components/PullsTable/PullsTable.jsx";
 import "./ReportPage.scss";
 
@@ -108,6 +107,7 @@ const ReportPage = () => {
               pullsArray={pullsArray}
             />
           </section>
+
           <section className="report__section">
             <div className="report__pulls-heading">
               <h2 className="report__subheading">
@@ -124,16 +124,6 @@ const ReportPage = () => {
                 Show prog pulls only
               </label>
             </div>
-
-            <ul className="report__pulls-list">
-              {progPullsOnly
-                ? getProgPulls().map((pull) => {
-                    return <Pull key={pull.id} pullData={pull} />;
-                  })
-                : pullsArray.map((pull) => {
-                    return <Pull key={pull.id} pullData={pull} />;
-                  })}
-            </ul>
 
             {progPullsOnly ? (
               <PullsTable pullsArray={getProgPulls()} />
