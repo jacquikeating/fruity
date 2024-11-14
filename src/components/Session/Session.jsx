abadb0ef68a478d50b20dc15ecccebc0a267065c
jacquikeating <j.keating@live.ca>
2024-11-14 16:34:55 -0500

    Session component renders basic data
    - Added an <h3> for the session number and a <p> element for each piece of data I plan to display (date, prog point, roster, and links).

--- src/components/Session/Session.jsx
+++ src/components/Session/Session.jsx
@@ -3,11 +3,34 @@ import { numSuffix } from "../../utils/shared-functions";
 import "./Session.scss";
 
 const Session = ({ sessionData }) => {
-  const { id, date, roster, prog_mech, prog_phase, fflogs_link, twitch_link } =
+  const { id, date, roster, prog_phase, prog_mech, fflogs_link, twitch_link } =
     sessionData;
   console.log(sessionData);
 
-  return <li className="session"></li>;
+  return (
+    <li className="session">
+      <div className="session__header">
+        <h3 className="session__title">Session {id}</h3>
+        <p className="session__date">{date}</p>
+      </div>
+      <p className="session__body-text">
+        <span className="session__category">Prog Point: </span>
+        Phase {prog_phase}, {prog_mech}
+      </p>
+      <p className="session__body-text">
+        <span className="session__category">Roster: </span>
+        {roster}
+      </p>
+      <p className="session__body-text">
+        <a className="session__link" href={fflogs_link}>
+          FFLogs
+        </a>
+        <a className="session__link" href={twitch_link}>
+          Twitch
+        </a>
+      </p>
+    </li>
+  );
 };
 
 export default Session;
