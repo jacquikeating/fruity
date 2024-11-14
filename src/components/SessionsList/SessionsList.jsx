b6bcc92a2d48620f34120a7c1fe465610f26e69f
jacquikeating <j.keating@live.ca>
2024-11-14 15:23:56 -0500

    Updated imports
    - Imported Session into SessionsList.
    - Imported SessionsList into OverviewPage.

--- src/components/SessionsList/SessionsList.jsx
+++ src/components/SessionsList/SessionsList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Session from "../Session/Session";
 import "./SessionsList.scss";
 
 const SessionsList = ({ sessionsArray }) => {
   return (
     <ul className="sessions-list">
       {sessionsArray.map((session) => {
-        // return <Session key={session.id} sessionData={session} />;
+        return <Session key={session.id} sessionData={session} />;
       })}
     </ul>
   );
--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SessionsList from "../../components/SessionsList/SessionsList";
 import "./OverviewPage.scss";
 
 const OverviewPage = () => {
