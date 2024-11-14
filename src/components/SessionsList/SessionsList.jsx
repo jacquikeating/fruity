f2df0ed2d137a583e7441c1dfb519b48bd6c53a6
jacquikeating <j.keating@live.ca>
2024-11-14 16:12:14 -0500

    SessionsList fetches data from API
    - Imported useState and useEffect hooks.
    - Imported axios.
    - Created sessionsArray state.
    - Copied functions to fetch data from the database, modifying to return an array of all sessions.
    - Currently, all it does is console.log() the returned data.

--- src/components/SessionsList/SessionsList.jsx
+++ src/components/SessionsList/SessionsList.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import Session from "../Session/Session";
 import "./SessionsList.scss";
 
-const SessionsList = ({ sessionsArray }) => {
+const SessionsList = () => {
+  const [sessionsArray, setSessionsArray] = useState([]);
+
+  useEffect(() => {
+    async function getSessionsData() {
+      try {
+        let result = await axios.get(`http://localhost:5050/sessions/`);
+        let data = result.data;
+        console.log(data);
+        setSessionsArray(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getSessionsData();
+  }, []);
+
   return (
     <ul className="sessions-list">
       {sessionsArray.map((session) => {
