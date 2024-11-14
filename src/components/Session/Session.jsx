01a02d24d026a6b2b74ca3aa8082452ccb5e7ec3
jacquikeating <j.keating@live.ca>
2024-11-14 14:43:31 -0500

    More renaming
    - Pull folder renamed to Session.
    - Prop passed to Session component renamed to sessionData.
    - Updated classNames.
    - Deleted old code.

---
+++ src/components/Session/Session.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { numSuffix } from "../../utils/shared-functions";
+import "./Session.scss";
+
+const Session = ({ sessionData }) => {
+  return <li className="session"></li>;
+};
+
+export default Session;
