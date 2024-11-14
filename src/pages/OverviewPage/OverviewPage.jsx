71c7224fbe9741ebe56d3199378024c918708455
jacquikeating <j.keating@live.ca>
2024-11-14 17:14:15 -0500

    Added basic structure to OverviewPage
    - Previously, OverviewPage was literally just returning <SessionsList>!
    - Added a <main> and an <h1> to provide some structure to the page.
    - Global styles on <main> and <h1> have instantly upgraded the appearance of the page, too.

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -3,7 +3,12 @@ import SessionsList from "../../components/SessionsList/SessionsList";
 import "./OverviewPage.scss";
 
 const OverviewPage = () => {
-  return <SessionsList />;
+  return (
+    <main className="overview-page">
+      <h1 className="overview-page__title">Overview</h1>
+      <SessionsList />
+    </main>
+  );
 };
 
 export default OverviewPage;
