bc84ec948babd3db58e61ba6a1130fb0ddfd782e
jacquikeating <j.keating@live.ca>
2024-11-17 13:58:18 -0500

    Made basic HTML structure for NewSessionForm
    - Added a <form>, <inputs>, and a <button> for the basic fields required from NewSessionForm.

--- src/components/NewSessionForm/NewSessionForm.jsx
+++ src/components/NewSessionForm/NewSessionForm.jsx
@@ -2,7 +2,47 @@ import React from "react";
 import "./NewSessionForm.scss";
 
 const NewSessionForm = () => {
-  return <p>NewSessionForm</p>;
+  return (
+    <form>
+      <label className="form__label" htmlFor="session-num">
+        Session #
+        <input
+          className="form__input form__input--number"
+          type="number"
+          name="session-num"
+          id="session-num"
+        />
+      </label>
+      <label className="form__label" htmlFor="roster">
+        Roster
+        <input
+          className="form__input form__input--text"
+          type="text"
+          name="roster"
+          id="roster"
+        />
+      </label>
+      <label className="form__label" htmlFor="prog-phase">
+        Prog Phase
+        <input
+          className="form__input form__input--number"
+          type="number"
+          name="prog-phase"
+          id="prog-phase"
+        />
+      </label>
+      <label className="form__label" htmlFor="prog-mech">
+        Prog Mech
+        <input
+          className="form__input form__input--text"
+          type="text"
+          name="prog-mech"
+          id="prog-mech"
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default NewSessionForm;
