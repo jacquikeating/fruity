02fca8230bc6140fbe9dccf04e1f4e950de49ab0
jacquikeating <j.keating@live.ca>
2024-11-17 12:58:48 -0500

    Created <section> elements for AddDataPage
    - Created 4 <sections> for AddDataPage: "New Session", "Session Info," "Add a Pull," and "Pulls List."
    - On initial load, only New Session will display. This will be a <form> component containing inputs for session data (date, number, roster, prog point).
    - Once the form is submitted, this section will be hidden and the other 3 will be displayed instead.
    - Session Info will contain the data entered in the <form>.
    - Add a Pull will contain <NewPullForm>.
    - Each time <NewPullForm> is submitted, the submitted data will be added to the Pulls section.

--- src/pages/AddDataPage/AddDataPage.jsx
+++ src/pages/AddDataPage/AddDataPage.jsx
@@ -6,7 +6,19 @@ const AddDataPage = () => {
   return (
     <main className="add-data">
       <h1 className="add-data__heading">Add Data</h1>
-      <NewPullForm />
+      <section className="add-data__section">
+        <h2 className="add-data__section-heading">New Session</h2>
+      </section>
+      <section className="add-data__section">
+        <h2 className="add-data__section-heading">Session Info</h2>
+      </section>
+      <section className="add-data__section">
+        <h2 className="add-data__section-heading">Add a Pull</h2>
+        <NewPullForm />
+      </section>
+      <section className="add-data__section">
+        <h2 className="add-data__section-heading">Pulls</h2>
+      </section>
     </main>
   );
 };
