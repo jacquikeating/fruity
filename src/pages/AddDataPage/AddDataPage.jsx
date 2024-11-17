f915cd0a8d6eb900d193df10781fe46369d60759
jacquikeating <j.keating@live.ca>
2024-11-17 13:09:24 -0500

    Added sessionInProgress state and conditional rendering
    - Added a boolean state, sessionInProgress, which initializes as false.
    - While false, only the New Session <section> is rendered on the page.
    - Submitting the session info form will switch the sessionInProgress state to true.
    - While true, the session info form will be hidden and the other 3 sections will be rendered.

--- src/pages/AddDataPage/AddDataPage.jsx
+++ src/pages/AddDataPage/AddDataPage.jsx
@@ -5,24 +5,30 @@ import "./AddDataPage.scss";
 
 const AddDataPage = () => {
   const [pullsArray, setPullsArray] = useState([]);
+  const [sessionInProgress, setSessionInProgress] = useState(false);
 
   return (
     <main className="add-data">
       <h1 className="add-data__heading">Add Data</h1>
-      <section className="add-data__section">
-        <h2 className="add-data__section-heading">New Session</h2>
-      </section>
-      <section className="add-data__section">
-        <h2 className="add-data__section-heading">Session Info</h2>
-      </section>
-      <section className="add-data__section">
-        <h2 className="add-data__section-heading">Add a Pull</h2>
-        <NewPullForm />
-      </section>
-      <section className="add-data__section">
-        <h2 className="add-data__section-heading">Pulls</h2>
-        <PullsTable pullsArray={pullsArray} />
-      </section>
+      {!sessionInProgress ? (
+        <section className="add-data__section">
+          <h2 className="add-data__section-heading">New Session</h2>
+        </section>
+      ) : (
+        <>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Session Info</h2>
+          </section>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Add a Pull</h2>
+            <NewPullForm />
+          </section>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Pulls</h2>
+            <PullsTable pullsArray={pullsArray} />
+          </section>
+        </>
+      )}
     </main>
   );
 };
