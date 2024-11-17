0b06917f388e299e9f12c2b21845b1621e5911f3
jacquikeating <j.keating@live.ca>
2024-11-17 15:54:05 -0500

    Added state variables for inputs
    - Imported useState into NewSessionForm.
    - Added state variables for each input: num, roster, progPhase, and progMech. Date will be auto-generated.

--- src/components/NewSessionForm/NewSessionForm.jsx
+++ src/components/NewSessionForm/NewSessionForm.jsx
@@ -1,47 +1,60 @@
-import React from "react";
+import { React, useState } from "react";
 import "./NewSessionForm.scss";
 
 const NewSessionForm = () => {
+  const [num, setNum] = useState("");
+  const [roster, setRoster] = useState("");
+  const [progPhase, setProgPhase] = useState("");
+  const [progMech, setProgMech] = useState("");
+
   return (
-    <form>
-      <label className="form__label" htmlFor="session-num">
-        Session #
-        <input
-          className="form__input form__input--number"
-          type="number"
-          name="session-num"
-          id="session-num"
-        />
-      </label>
-      <label className="form__label" htmlFor="roster">
-        Roster
-        <input
-          className="form__input form__input--text"
-          type="text"
-          name="roster"
-          id="roster"
-        />
-      </label>
-      <label className="form__label" htmlFor="prog-phase">
-        Prog Phase
-        <input
-          className="form__input form__input--number"
-          type="number"
-          name="prog-phase"
-          id="prog-phase"
-        />
-      </label>
-      <label className="form__label" htmlFor="prog-mech">
-        Prog Mech
-        <input
-          className="form__input form__input--text"
-          type="text"
-          name="prog-mech"
-          id="prog-mech"
-        />
-      </label>
       <button type="submit">Submit</button>
-    </form>
+      <form>
+        <label className="form__label" htmlFor="session-num">
+          Session #
+          <input
+            className="form__input form__input--number"
+            type="number"
+            name="session-num"
+            id="session-num"
+            value={num}
+            onChange={(e) => setNum(e.target.value)}
+          />
+        </label>
+        <label className="form__label" htmlFor="roster">
+          Roster
+          <input
+            className="form__input form__input--text"
+            type="text"
+            name="roster"
+            id="roster"
+            value={roster}
+            onChange={(e) => setRoster(e.target.value)}
+          />
+        </label>
+        <label className="form__label" htmlFor="prog-phase">
+          Prog Phase
+          <input
+            className="form__input form__input--number"
+            type="number"
+            name="prog-phase"
+            id="prog-phase"
+            value={progPhase}
+            onChange={(e) => setProgPhase(e.target.value)}
+          />
+        </label>
+        <label className="form__label" htmlFor="prog-mech">
+          Prog Mech
+          <input
+            className="form__input form__input--text"
+            type="text"
+            name="prog-mech"
+            id="prog-mech"
+            value={progMech}
+            onChange={(e) => setProgMech(e.target.value)}
+          />
+        </label>
+      </form>
   );
 };
 
