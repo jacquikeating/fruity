889113e90089c8b5d4ddbd5756e5804096f1b932
jacquikeating <j.keating@live.ca>
2024-11-24 15:07:17 -0500

    Added POST route for new pulls
    - Copied addSession() function from sessionController and pasted into pullController, with names adjusted to match.
    - Updated error messages for all functions in both pullController and sessionController, as they still mentioned warehouses, which we don't have in this project... oops.
    - Added a .post route to pullController's root route.

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -19,7 +19,7 @@ export const singleSession = async (req, res) => {
     res
       .status(400)
       .send(
-        `Error retrieving inventories for Warehouse ${req.params.sessionID}: ${err}`
+        `Error retrieving data for session ${req.params.sessionID}: ${err}`
       );
   }
 };
@@ -32,7 +32,7 @@ export const sessionPulls = async (req, res) => {
     res
       .status(400)
       .send(
-        `Error retrieving inventories for Warehouse ${req.params.sessionID}: ${err}`
+        `Error retrieving pulls data for session ${req.params.sessionID}: ${err}`
       );
   }
 };
@@ -44,6 +44,6 @@ export const addSession = async (req, res) => {
     const newSessionURL = `/report/${data[0]}`;
     res.status(201).location(newSessionURL).end(newSessionURL);
   } catch (err) {
-    res.status(400).send(`Error creating Warehouse: ${err}`);
+    res.status(400).send(`Error creating session: ${err}`);
   }
 };
