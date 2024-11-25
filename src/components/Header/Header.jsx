5f6234353a254f8d0e3191e6c16b1f3341adf4ae
jacquikeating <j.keating@live.ca>
2024-11-25 18:48:37 -0500

    Added first links
    - Imported Link from react-router-dom.
    - Implemented two links, to OverviewPage and AddDataPage respectively.
    - Applied initial styles.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="nav">
-        <ul className="nav__list"></ul>
+        <ul className="nav__list">
+          <li className="nav__list-item">
+            <Link to="/" className="nav__link">
+              Home
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/add-data" className="nav__link">
+              Add Data
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
