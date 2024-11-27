3b5ffc93efc63ccd3dd69036979aebd4b3c5fb9b
jacquikeating <j.keating@live.ca>
2024-11-27 14:56:56 -0500

    Created editMode state
    - Imported useState.
    - Created a new state variable, editMode, which initializes to false.
    - Created editRow() function, which sets editMode to true.
    - Added an onClick listener to the edit button, which triggers editRow().
    - Added conditionals to return different content depending on the state of editMode. When false (the default), the regular table row is displayed. When true, all content is replaced with "EDIT MODE" for testing purposes. In full implementation, all text will be replaced with inputs.

--- src/components/Pull/Pull.jsx
+++ src/components/Pull/Pull.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Pull.scss";
 
 const Pull = ({ pullData, pullNumType, showEdit }) => {
+  const [editMode, setEditMode] = useState(false);
+
   const {
     id,
     pull_num_today,
@@ -15,39 +17,80 @@ const Pull = ({ pullData, pullNumType, showEdit }) => {
     index,
   } = pullData;
 
-  return (
-    <tr key={`pull-${id}`} className="pulls-table__row">
-      <td
-        key={`#${id}-${pull_num_today}`}
-        className="pulls-table__cell pulls-table__cell--num-today"
-      >
-        {pullNumType === "today" ? index + 1 : id}
-      </td>
-      <td key={`${id}-${phase}`} className="pulls-table__cell">
-        {phase}
-      </td>
-      <td key={`${id}-${mech}`} className="pulls-table__cell">
-        {mech}
-      </td>
-      <td key={`${id}-${cause}`} className="pulls-table__cell">
-        {cause}
-      </td>
-      <td key={`${id}-${players_responsible}`} className="pulls-table__cell">
-        {players_responsible?.join(", ")}
-      </td>
-      <td key={`${id}-${notes}`} className="pulls-table__cell">
-        {notes}
-      </td>
-      {showEdit ? (
-        <td key={`${id}-actions`} className="pulls-table__cell">
-          <button>Edit</button>
-          <button>Delete</button>
+  function editRow() {
+    setEditMode(true);
+    console.log("Editing enabled.");
+  }
+
+  if (!editMode) {
+    return (
+      <tr key={`pull-${id}`} className="pulls-table__row">
+        <td
+          key={`#${id}-${pull_num_today}`}
+          className="pulls-table__cell pulls-table__cell--num-today"
+        >
+          {pullNumType === "today" ? index + 1 : id}
         </td>
-      ) : (
-        ""
-      )}
-    </tr>
-  );
+        <td key={`${id}-${phase}`} className="pulls-table__cell">
+          {phase}
+        </td>
+        <td key={`${id}-${mech}`} className="pulls-table__cell">
+          {mech}
+        </td>
+        <td key={`${id}-${cause}`} className="pulls-table__cell">
+          {cause}
+        </td>
+        <td key={`${id}-${players_responsible}`} className="pulls-table__cell">
+          {players_responsible?.join(", ")}
+        </td>
+        <td key={`${id}-${notes}`} className="pulls-table__cell">
+          {notes}
+        </td>
+        {showEdit ? (
+          <td key={`${id}-actions`} className="pulls-table__cell">
+            <button onClick={editRow}>Edit</button>
+            <button>Delete</button>
+          </td>
+        ) : (
+          ""
+        )}
+      </tr>
+    );
+  } else {
+    return (
+      <tr key={`pull-${id}`} className="pulls-table__row">
+        <td
+          key={`#${id}-${pull_num_today}`}
+          className="pulls-table__cell pulls-table__cell--num-today"
+        >
+          EDIT MODE
+        </td>
+        <td key={`${id}-${phase}`} className="pulls-table__cell">
+          EDIT MODE
+        </td>
+        <td key={`${id}-${mech}`} className="pulls-table__cell">
+          EDIT MODE
+        </td>
+        <td key={`${id}-${cause}`} className="pulls-table__cell">
+          EDIT MODE
+        </td>
+        <td key={`${id}-${players_responsible}`} className="pulls-table__cell">
+          EDIT MODE
+        </td>
+        <td key={`${id}-${notes}`} className="pulls-table__cell">
+          EDIT MODE
+        </td>
+        {showEdit ? (
+          <td key={`${id}-actions`} className="pulls-table__cell">
+            <button onClick={editRow}>Edit</button>
+            <button>Delete</button>
+          </td>
+        ) : (
+          ""
+        )}
+      </tr>
+    );
+  }
 };
 
 export default Pull;
