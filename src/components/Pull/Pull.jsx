96e5265a5308885a000d80899a587b23f789d596
jacquikeating <j.keating@live.ca>
2024-11-27 14:35:34 -0500

    Copied content into Pull component
    - Copied all of PullsTable's JSX used to create an individual <tr> into Pull component.

--- src/components/Pull/Pull.jsx
+++ src/components/Pull/Pull.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import "./Pull.scss";
 
-const Pull = () => {
-  return <div>Pull</div>;
+const Pull = (pullData) => {
+  const {
+    id,
+    pull_num_today,
+    phase,
+    mech,
+    cause,
+    players_responsible,
+    log_link,
+    clip_link,
+    notes,
+  } = pullData;
+
+  return (
+    <tr key={`pull-${id}`} className="pulls-table__row">
+      <td
+        key={`#${id}-${pull_num_today}`}
+        className="pulls-table__cell pulls-table__cell--num-today"
+      >
+        {pullNumType === "today" ? index + 1 : id}
+      </td>
+      <td key={`${id}-${phase}`} className="pulls-table__cell">
+        {phase}
+      </td>
+      <td key={`${id}-${mech}`} className="pulls-table__cell">
+        {mech}
+      </td>
+      <td key={`${id}-${cause}`} className="pulls-table__cell">
+        {cause}
+      </td>
+      <td key={`${id}-${players_responsible}`} className="pulls-table__cell">
+        {players_responsible?.join(", ")}
+      </td>
+      <td key={`${id}-${notes}`} className="pulls-table__cell">
+        {notes}
+      </td>
+      {showEdit ? (
+        <td key={`${id}-actions`} className="pulls-table__cell">
+          <button>Edit</button>
+          <button>Delete</button>
+        </td>
+      ) : (
+        ""
+      )}
+    </tr>
+  );
 };
 
 export default Pull;
