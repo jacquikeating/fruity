0ae5f3922b4f49bf36ee980cbe3ff90185c25e2c
jacquikeating <j.keating@live.ca>
2024-11-30 16:06:39 -0500

    Added delete routes
    - Created a deletePull() funciton in pullController.js.
    - Created a route to delete a pull, using the pullID param, to routes/pulls.js.

--- controllers/pullController.js
+++ controllers/pullController.js
@@ -10,11 +10,32 @@ export const index = async (_req, res) => {
     res.status(400).send(`Error retrieving data: ${err}`);
   }
 };
+
 export const addPull = async (req, res) => {
   try {
     const data = await knex("pull").insert(req.body);
     res.status(201).send(data);
   } catch (err) {
     res.status(400).send(`Error creating pull: ${err}`);
+  }
+};
+
+export const deletePull = async (req, res) => {
+  const { pullID } = req.params;
+
+  try {
+    const pull = await knex("pull").where({ id: pullID }).first();
+
+    if (!pull) {
+      return res.status(404).json({
+        error_code: 404,
+        error_msg: `Pull with ID ${pullID} not found.`,
+      });
+    }
+
+    await knex("pull").where({ id: pullID }).del();
+    res.status(204).send();
+  } catch (err) {
+    res.status(400).send(`Error deleting pull: ${err}`);
   }
 };
--- routes/pulls.js
+++ routes/pulls.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.route("/").get(pullController.index).post(pullController.addPull);
 
+router.route("/:pullID").delete(pullController.deletePull);
 export default router;
