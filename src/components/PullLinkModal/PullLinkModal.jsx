7b4c21bf38daac734fa1667d04bd08e0f9f13301
jacquikeating <j.keating@live.ca>
2024-12-05 12:26:12 -0500

    Added HTML content to PullLinkModal
    - Just a <div> with an <input> and <label> for each link type. The <labels> are simply icons with alt text.

--- src/components/PullLinkModal/PullLinkModal.jsx
+++ src/components/PullLinkModal/PullLinkModal.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./PullLinkModal.scss";
 
 const PullLinkModal = () => {
-  return <div>PullLinkModal</div>;
+  return (
+    <div>
+      <label htmlFor="link-input-twitch">
+        <img src="https://i.imgur.com/NzRUemQ.png" alt="Twitch" />
+      </label>
+      <input id="link-input-twitch" type="text"></input>
+      <label htmlFor="link-input-fflogs">
+        <img src="https://i.imgur.com/NzRUemQ.png" alt="FFLogs" />
+      </label>
+      <input id="link-input-fflogs" type="text" />
+    </div>
+  );
 };
 
 export default PullLinkModal;
