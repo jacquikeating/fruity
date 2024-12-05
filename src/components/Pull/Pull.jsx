fd519fe34f4686268aede7671d7ba19557c2827d
jacquikeating <j.keating@live.ca>
2024-12-05 12:38:54 -0500

    Moved link code from Pull to PullLink
    - Cut and pasted all link-related JSX to PullLink component.
    - Dealing with states in next commit.

--- src/components/Pull/Pull.jsx
+++ src/components/Pull/Pull.jsx
@@ -130,41 +130,6 @@ const Pull = ({
             }}
           />
         )}
-
-        {!editMode ? (
-          ""
-        ) : (
-          <button className="pull__add-link-btn" onClick={toggleLinkModal}>
-            +
-          </button>
-        )}
-
-        {clipLink ? (
-          <a
-            className="pull__link"
-            href={clipLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="https://i.imgur.com/HRxy3mm.png" className="pull__icon" />
-          </a>
-        ) : (
-          ""
-        )}
-        {logLink ? (
-          <a
-            className="pull__link"
-            href={logLink}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="https://i.imgur.com/WUA83tW.png" className="pull__icon" />
-          </a>
-        ) : (
-          ""
-        )}
-
-        {showModal ? <EditLinkModal /> : ""}
       </td>
 
       {showEdit ? (
--- src/components/PullLink/PullLink.jsx
+++ src/components/PullLink/PullLink.jsx
@@ -2,7 +2,44 @@ import React from "react";
 import "./PullLink.scss";
 
 const PullLink = () => {
-  return <div>PullLink</div>;
+  return (
+    <div>
+      {!editMode ? (
+        ""
+      ) : (
+        <button className="pull__add-link-btn" onClick={toggleLinkModal}>
+          +
+        </button>
+      )}
+
+      {clipLink ? (
+        <a
+          className="pull__link"
+          href={clipLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="https://i.imgur.com/HRxy3mm.png" className="pull__icon" />
+        </a>
+      ) : (
+        ""
+      )}
+      {logLink ? (
+        <a
+          className="pull__link"
+          href={logLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="https://i.imgur.com/WUA83tW.png" className="pull__icon" />
+        </a>
+      ) : (
+        ""
+      )}
+
+      {showModal ? <EditLinkModal /> : ""}
+    </div>
+  );
 };
 
 export default PullLink;
