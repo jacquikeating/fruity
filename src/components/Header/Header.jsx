d3b230cb07ef1fa729943be3563b67ce4c462ffb
jacquikeating <j.keating@live.ca>
2024-12-04 20:55:29 -0500

    Header styling
    - Added a lighter background colour for the header (and even made a new variable for it).
    - Added a logo.
    - Adjusted spacing and baselines to align logo with links.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,25 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav">
+        <Link to="/" className="nav__site-logo">
+          <img src="https://i.imgur.com/GNPU7aE.png"></img>
+          <p className="nav__site-name">wall is FRUity</p>
+        </Link>
+
         <ul className="nav__list">
           <li className="nav__list-item">
             <Link to="/" className="nav__link">
               Home
             </Link>
           </li>
-          <li className="nav__list-item">
+          {/* <li className="nav__list-item">
             <Link to="/add-data" className="nav__link">
               Add Data
+            </Link>
+          </li> */}
+          <li className="nav__list-item">
+            <Link to="/about" className="nav__link">
+              About
             </Link>
           </li>
         </ul>
