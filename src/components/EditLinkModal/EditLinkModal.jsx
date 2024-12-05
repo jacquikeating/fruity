52ad8487c3cfbaddd3dae3c5890b8929c1b150ea
jacquikeating <j.keating@live.ca>
2024-12-05 13:13:53 -0500

    Applied BEM classNames to EditLinkModal elements

--- src/components/EditLinkModal/EditLinkModal.jsx
+++ src/components/EditLinkModal/EditLinkModal.jsx
@@ -3,15 +3,27 @@ import "./EditLinkModal.scss";
 
 const EditLinkModal = () => {
   return (
-    <div>
-      <label htmlFor="link-input-twitch">
-        <img src="https://i.imgur.com/NzRUemQ.png" alt="Twitch" />
+    <div className="link-modal">
+      <label htmlFor="link-input-twitch" className="link-modal__label">
+        <img
+          src="https://i.imgur.com/NzRUemQ.png"
+          alt="Twitch"
+          className="link-modal__icon"
+        />
       </label>
-      <input id="link-input-twitch" type="text"></input>
-      <label htmlFor="link-input-fflogs">
-        <img src="https://i.imgur.com/asZe3Wu.png" alt="FFLogs" />
+      <input
+        id="link-input-twitch"
+        type="text"
+        className="link-modal__input"
+      ></input>
+      <label htmlFor="link-input-fflogs" className="link-modal__label">
+        <img
+          src="https://i.imgur.com/asZe3Wu.png"
+          alt="FFLogs"
+          className="link-modal__icon"
+        />
       </label>
-      <input id="link-input-fflogs" type="text" />
+      <input id="link-input-fflogs" type="text" className="link-modal__input" />
     </div>
   );
 };
