22a27b28c3ee311f8ae68b102c5104a6eee407c1
jacquikeating <j.keating@live.ca>
2024-12-07 12:57:34 -0500

    Seed data uses strings instead of JSON.stringifying arrays
    - If I can't even use JSON data type anymore (THANKS A LOT, JAWSDB), why even bring JSON into it?
    - I have a lot of now-unnecessary code switching strings to arrays and back to strings and back to arrays... so I am now refactoring code to stick with fake-array strings as the default, and .split(", ") them into actual arrays only within the functions that need them to be arrays.
    - Rather than taking arrays and JSON.stringifying them, I am now storing all array-type data as simple strings. The old data had to be converted, but new inputs will be added as basic strings, no splitting or joining or stringifying required.

--- seed_data/session.js
+++ seed_data/session.js
@@ -4,44 +4,22 @@ export default [
     prog_phase: 1,
     prog_mech: "Opener",
     fflogs_link: "https://www.fflogs.com/reports/Fn6zbtjc2hLpVN9M",
-    twitch_links: JSON.stringify([
-      "https://www.twitch.tv/videos/2312032144",
-      "https://www.twitch.tv/videos/2312034395",
-    ]),
-    roster: JSON.stringify([
-      "Chro",
-      "Sophia",
-      "Ruv",
-      "Ella",
-      "Quil",
-      "Tori",
-      "Char",
-      "Laveera",
-    ]),
+    twitch_links:
+      "https://www.twitch.tv/videos/2312032144, https://www.twitch.tv/videos/2312034395",
+    roster: "Chro, Sophia, Ruv, Ella, Quil, Tori, Char, Laveera",
+
     goal: "Die a lot.",
-    notes: JSON.stringify([
-      "Unofficial session 0",
-      "Chro's VOD is split into multiple parts",
-      "Should we even count this session?",
-    ]),
+    notes:
+      "Unofficial session 0, Chro's VOD is split into multiple parts, Should we even count this session?",
   },
   {
     date: "2024-12-04T21:00:00Z",
     prog_phase: 1,
     prog_mech: "Enrage",
     fflogs_link: "https://www.fflogs.com/reports/4W9pdzBFkvZn8CGc",
-    twitch_links: JSON.stringify(["https://www.twitch.tv/videos/2318554836"]),
-    roster: JSON.stringify([
-      "Chro",
-      "Sophia",
-      "Ruv",
-      "Ella",
-      "Quil",
-      "Hypatia",
-      "Char",
-      "Laveera",
-    ]),
+    twitch_links: "https://www.twitch.tv/videos/2318554836",
+    roster: "Chro, Sophia, Ruv, Ella, Quil, Hypatia, Char, Laveera",
     goal: "Clear phase 1",
-    notes: JSON.stringify([""]),
+    notes: "",
   },
 ];
