e858ad465bb49cb9dd9c17b4f9534e5d5da3c353
jacquikeating <j.keating@live.ca>
2024-12-06 19:21:22 -0500

    Removed double JSON.stringify
    - Perhaps this is the problem? In the pull seed file (temporarily truncated for testing, because it's easier to find syntax errors in one pull than 98), I was JSON.stringifying players_responsible TWICE.

--- seed_data/pull.js
+++ seed_data/pull.js
@@ -7,7 +7,7 @@ export default [
     mech: "Utopian Sky",
     prog_point_reached: "target",
     cause: "No tank swap on 1st TB",
-    players_responsible: JSON.stringify(JSON.stringify(["Chro", "Sophia"])),
+    players_responsible: JSON.stringify(["Chro", "Sophia"]),
     log_link: "",
     clip_link: "",
     notes: "Next auto killed Chro → Chro exploded and killed 3 others",
