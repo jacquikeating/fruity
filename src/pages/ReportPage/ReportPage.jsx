fafba58b37dc5754e20be815980bd21cf240741f
jacquikeating <j.keating@live.ca>
2024-12-10 09:20:46 -0500

    sessionID in useEffect
    - Testing to see if adding sessionID to the useEffect arguments will trigger the page to update when Latest is clicked.

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -46,7 +46,7 @@ const ReportPage = () => {
 
     getSessionData();
     getPullsData();
-  }, []);
+  }, [sessionID]);
 
   function getProgPulls() {
     const filteredPullsArray = pullsArray.filter(
