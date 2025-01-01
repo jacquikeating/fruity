a7ebcb5e52f629f27d306ed12a7a2ab3ce85f617
jacquikeating <j.keating@live.ca>
2024-12-31 20:59:39 -0500

    Added mechsList to MechsPage
    - Added an array with all mechanics to MechsPage.
    - Removed "Akh Morns" from NewPullForm as I felt it wasn't a full mechanic on its own, but part of Darklit Dragonsong.
    - Corrected a typo in "Polarizing Strikes."
    - Corrected typo in MechsPage name (it was still MechPage).

--- src/pages/MechsPage/MechPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./MechsPage.scss";
-
-const MechsPage = () => {
-  return <div>MechsPage</div>;
-};
-
-export default MechsPage;
---
+++ src/pages/MechsPage/MechsPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./MechsPage.scss";
+
+const MechsPage = () => {
+  const mechsList = [
+    "P1 Opener",
+    "Utopian Sky",
+    "Fall of Faith",
+    "Towers",
+    "P1 Enrage",
+    "Diamond Dust",
+    "Mirrors",
+    "Light Rampant",
+    "P2 Enrage",
+    "Intermission",
+    "Ultimate Relativity",
+    "Apocalypse",
+    "P3 Enrage",
+    "Darklit Dragonsong",
+    "Crystallize Time",
+    "P4 Enrage",
+    "Fulgent Blade",
+    "Paradise Regained",
+    "Polarizing Strikes",
+    "P5 Enrage",
+  ];
+  return <div>MechsPage</div>;
+};
+
+export default MechsPage;
