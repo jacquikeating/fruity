e4c19a64fc3cfee91acd1a6339cc6a605d2d2ddc
jacquikeating <j.keating@live.ca>
2025-01-03 00:56:49 -0500

    Created getMechAfterProgMech() function
    - Need to include pulls beyond the prog point when the user filters to display prog pulls only.
    - Created a new function in shared-functions.js, getMechAfterProgMech().
    - This function takes the progMech as an argument, then finds its index in the mechsList array.
    - It then returns the next item in the array.

--- src/utils/shared-functions.js
+++ src/utils/shared-functions.js
@@ -138,3 +138,30 @@ export function checkIfEmptyLink(link) {
     return "empty-link";
   }
 }
+
+export function getMechAfterProgMech(progMech) {
+  const mechsList = [
+    "P1 Opener",
+    "Utopian Sky",
+    "Fall of Faith",
+    "Towers",
+    "P1 Enrage",
+    "Diamond Dust",
+    "Mirrors",
+    "Light Rampant",
+    "P2 Enrage",
+    "Intermission",
+    "Ultimate Relativity",
+    "Apocalypse",
+    "P3 Enrage",
+    "Darklit Dragonsong",
+    "Crystallize Time",
+    "P4 Enrage",
+    "Fulgent Blade",
+    "Paradise Regained",
+    "Polarizing Strikes",
+    "P5 Enrage",
+  ];
+  const progMechIndex = mechsList.indexOf(progMech);
+  return mechsList[progMechIndex + 1];
+}
