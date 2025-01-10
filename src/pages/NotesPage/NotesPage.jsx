03d23efbecf1f2c8eaa272943b3a862e0756988c
jacquikeating <j.keating@live.ca>
2025-01-10 16:29:37 -0500

    Added basic content
    - Created <main>, <h1>, <section>, <h2>, and <p> elements to serve as introductory content for NotesPage.

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./NotesPage.scss";
 
 const NotesPage = () => {
-  return <div>NotesPage</div>;
+  return (
+    <main className="notes">
+      <h1 className="notes__heading">Notes</h1>
+      <section className="notes__section">
+        <h2 className="notes__subheading">Darklit Dragonsong</h2>
+        <p className="notes__body-text">
+          When I am studying, I find it helpful not only to watch guide videos
+          where everything is perfectly executed, but to also see clips where
+          something goes wrong. So here's a collection of all the different ways
+          you can screw up Darklit Dragonsong!
+        </p>
+        <p className="notes__body-text">
+          The point here is not to shame random strangers (who are probably more
+          skilled than me) — it's to learn common pitfalls to look out for in my
+          own prog.
+        </p>
+      </section>
+    </main>
+  );
 };
 
 export default NotesPage;
