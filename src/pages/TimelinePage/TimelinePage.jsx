116bf1ea1ce733992c8166b71624abf1efdece90
jacquikeating <j.keating@live.ca>
2025-01-17 21:09:11 -0500

    Styled TimelinePage
    - Added styles to TimelinePage, based on those used in PullsTable.
    - Adjusted classNames where needed for styling purposes.

--- src/pages/TimelinePage/TimelinePage.jsx
+++ src/pages/TimelinePage/TimelinePage.jsx
@@ -13,35 +13,45 @@ const TimelinePage = () => {
 
   return (
     <main className="timeline">
-      <table className="timeline__table">
-        <tbody>
-          {phases.map((phase) => {
-            return (
-              <div>
-                <h2 className="timeline__subheading">
-                  {phase.phaseNum} - {phase.phaseName}
-                </h2>
-                {filterMechs(phase.phaseNum).map((mech) => {
-                  return (
-                    <tr className="timeline__row">
-                      <td className="timeline__cell timeline__cell--time">
-                        {mech.time}
-                      </td>
-                      <td className="timeline__cell timeline__cell--name">
-                        {mech.name}
-                      </td>
-                      <td className="timeline__cell timeline__cell--nick">
-                        {" "}
-                        {mech.nickname}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="timeline__container">
+        <table className="timeline__table">
+          <tbody>
+            {phases.map((phase) => {
+              return (
+                <>
+                  <tr className="timeline__header-row">
+                    <th className="timeline__header-cell timeline__header-cell--phase">
+                      {" "}
+                      <h2 className="timeline__subheading">
+                        {phase.phaseName}
+                      </h2>
+                    </th>
+                    <th className="timeline__header-cell timeline__header-cell--name"></th>
+                    <th className="timeline__header-cell timeline__header-cell--nick"></th>
+                  </tr>
+
+                  {filterMechs(phase.phaseNum).map((mech) => {
+                    return (
+                      <tr className="timeline__row">
+                        <td className="timeline__cell timeline__cell--time">
+                          {mech.time}
+                        </td>
+                        <td className="timeline__cell timeline__cell--name">
+                          {mech.name}
+                        </td>
+                        <td className="timeline__cell timeline__cell--nick">
+                          {" "}
+                          {mech.nickname}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 };
