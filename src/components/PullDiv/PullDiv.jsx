53f067a4defe6576c792fbde2d945384b8a27298
jacquikeating <j.keating@live.ca>
2025-01-27 23:58:14 -0500

    Early implementation of PullDiv
    - Added <p> elements for each piece of information to be displayed in PullDiv.
    - Currently no editing functionality.
    - Basic styling applied.

--- src/components/PullDiv/PullDiv.jsx
+++ src/components/PullDiv/PullDiv.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./PullDiv.scss";
 
-const PullDiv = () => {
-  return <div>PullDiv</div>;
+const PullDiv = ({
+  pullData,
+  showEdit,
+  updatePull,
+  deletePull,
+  progPhase,
+  allowDelete,
+}) => {
+  return (
+    <div className="pull-div">
+      <div className="pull-div__top">
+        <p className="pull-div__pull-num">{pullData.pull_num_today}</p>
+        <p className="pull-div__p">P{pullData.phase}</p>
+        <p className="pull-div__p">{pullData.mech}</p>
+      </div>
+      <div className="pull-div__mid">
+        <p className="pull-div__p">
+          ({pullData.players_responsible}) {pullData.cause}
+        </p>
+        <p className="pull-div__p pull-div__p--notes">{pullData.notes}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PullDiv;
