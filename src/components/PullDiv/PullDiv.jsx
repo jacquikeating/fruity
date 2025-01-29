46e94ff8d7a6da9d802011132293bbb594c3c2e5
jacquikeating <j.keating@live.ca>
2025-01-28 23:03:27 -0500

    PullDiv structural changes
    -

--- src/components/PullDiv/PullDiv.jsx
+++ src/components/PullDiv/PullDiv.jsx
@@ -14,14 +14,32 @@ const PullDiv = ({
       <p className="pull-div__pull-num">{pullData.pull_num_today}</p>
       <div className="pull-div__info">
         <div className="pull-div__top">
-          <p className="pull-div__p">P{pullData.phase}</p>
-          <p className="pull-div__p">{pullData.mech}</p>
+          <p className="pull-div__p pull-div__p--mech">
+            P{pullData.phase}
+            <span className="pull-div__divider">•</span>
+            {pullData.mech}
+          </p>
         </div>
+
         <div className="pull-div__mid">
           <p className="pull-div__p">
-            ({pullData.players_responsible}) {pullData.cause}
+            {/* <span className="pull-div__bold">Cause: </span> */}
+            {pullData.cause}
           </p>
-          <p className="pull-div__p pull-div__p--notes">{pullData.notes}</p>
+          {pullData.notes ? (
+            <p className="pull-div__p pull-div__p--notes">
+              <span className="pull-div__bold">Notes: </span>
+              {pullData.notes}
+            </p>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div className="pull-div__bottom">
+          <p className="pull-div__p pull-div__p--players">
+            {pullData.players_responsible}
+          </p>
         </div>
       </div>
     </div>
