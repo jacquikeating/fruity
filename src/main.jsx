7048379a9c4f36f8b020a1b6354883279a07c897
jacquikeating <j.keating@live.ca>
2025-02-02 17:21:03 -0500

    Basic login implementation
    - Imported useAuth0 to main.jsx and LoginPage.jsx.
    - Nested <App /> inside <Auth0Provider>.
    - Created a simple <button> that calls loginWithRedirect() on click.
    - Tested login form. Working!

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import App from "./App.jsx";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-28ixr5eh7jkt3f04.ca.auth0.com"
+      clientId="4wqqQ23Y9I6zh4XK2UDQon4YWvFS9Oxs"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./LoginPage.scss";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated) {
+    return (
+      <main className="login">
+        <button className="login__button" onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      </main>
+    );
+  }
 };
 
 export default LoginPage;
