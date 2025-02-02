0af503f3a1fc77dad6b12326e91f7e40e032a7a7
jacquikeating <j.keating@live.ca>
2025-02-02 15:13:20 -0500

    Added login link to Header
    - Separated <nav> into two <div> elements, called nav__group.
    - Applied BEM classNames to distinguish primary (left side) nav items from secondary (right side) items.
    - Put a <ul> with an <li> for the link to LoginPage in the secondary group.
    - Applied styles, decreasing the font-size of secondary items.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,38 +6,49 @@ const Header = ({ latestSession }) => {
   return (
     <header className="header">
       <nav className="nav">
-        <Link to="/" className="nav__site-logo">
-          <img src="https://i.imgur.com/GNPU7aE.png"></img>
-          <p className="nav__site-name">wall is FRUity</p>
-        </Link>
+        <div className="nav__group nav__group--primary">
+          <Link to="/" className="nav__site-logo">
+            <img src="https://i.imgur.com/GNPU7aE.png"></img>
+            <p className="nav__site-name">wall is FRUity</p>
+          </Link>
 
-        <ul className="nav__list">
-          <li className="nav__list-item">
-            <Link to="/" className="nav__link">
-              Home
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link to={`/report/${latestSession}`} className="nav__link">
-              Latest
-            </Link>
-          </li>
-          {/* <li className="nav__list-item">
+          <ul className="nav__list">
+            <li className="nav__list-item">
+              <Link to="/" className="nav__link">
+                Home
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to={`/report/${latestSession}`} className="nav__link">
+                Latest
+              </Link>
+            </li>
+            {/* <li className="nav__list-item">
             <Link to="/add-data" className="nav__link">
               Add Data
             </Link>
           </li> */}
-          <li className="nav__list-item">
-            <Link to="/about" className="nav__link">
-              About
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link to="/clips" className="nav__link">
-              Clips
-            </Link>
-          </li>
-        </ul>
+            <li className="nav__list-item">
+              <Link to="/about" className="nav__link">
+                About
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/clips" className="nav__link">
+                Clips
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav__group nav__group--secondary">
+          <ul className="nav__list">
+            <li className="nav__list-item">
+              <Link to="/login" className="nav__link">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
