bd17f6ff4d317b3cea641fd9fa7fdddbbf7eb617
jacquikeating <j.keating@live.ca>
2025-02-02 22:36:42 -0500

    Fleshed out LoginPage a bit
    - Added additional stuff from useAuth0().
    - Added logout button.
    - Added "Loading..." text.
    - Added basic user account info.
    - Added a role variable to get the role assigned to the user, which will be used in other contexts to enable certain sensitive content.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,13 +3,41 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./LoginPage.scss";
 
 const LoginPage = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, isLoading, user } = useAuth0();
+  const { logout } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   if (!isAuthenticated) {
     return (
       <main className="login">
+        <h1 className="login__heading">Log In</h1>
         <button className="login__button" onClick={() => loginWithRedirect()}>
           Log In
+        </button>
+      </main>
+    );
+  } else {
+    const role = user["https://wall-is-safe.netlify.app/roles"][0];
+
+    return (
+      <main className="login">
+        <h1 className="login__heading">Your Account</h1>
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          {role === "admin" ? <p>Hi admin</p> : ""}
+        </div>
+        <button
+          className="login__button"
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          Log Out
         </button>
       </main>
     );
