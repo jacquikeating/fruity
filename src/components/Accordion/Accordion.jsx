b7750475c5769cd8aab2a2f58c0bb1468fc8d72d
jacquikeating <j.keating@live.ca>
2025-02-03 22:48:02 -0500

    Created folder and files for AccordionItem
    - Created folder and files for the child components that will be put into Accordion.
    - Imported AccordionItem into Accordion.

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AccordionItem from "../AccordionItem/AccordionItem";
 import "./Accordion.scss";
 
 const Accordion = () => {
---
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AccordionItem.scss";
+
+const AccordionItem = () => {
+  return <div>AccordionItem</div>;
+};
+
+export default AccordionItem;
