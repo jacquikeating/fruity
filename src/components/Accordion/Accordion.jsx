1aea09ccf35dfa0951806c1676198822b2d2e139
jacquikeating <j.keating@live.ca>
2025-02-07 20:50:34 -0500

    Added handleToggle(), onToggle(), and active prop
    - Created a function in Accordion, handleToggle(), which is passed down to AccordionItems to set the clicked state in the parent element.
    - Passed handleToggle() as a prop to the AccordionItem child components, named onToggle().
    - Set onToggle() as an onClick event on the AccordionItem header.
    - When an AccordionItem header is clicked, the handleToggle() function checks whether the index of the child element matches the current value of the clicked state. If yes (the item was already clicked), then it resets clicked to 0. If not, clicked is set to that index.
    - Passed active prop down to AccordionItem. If clicked matches the item's index, then active is true. While active is true, the caret has a modifier class applied which rotates the caret upwards.

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -6,10 +6,23 @@ import "./Accordion.scss";
 const Accordion = ({ accordionItemsData, context }) => {
   const [clicked, setClicked] = useState("0");
 
+  const handleToggle = (index) => {
+    if (clicked === index) {
+      return setClicked("0");
+    }
+    setClicked(index);
+  };
+
   return (
     <ul className="accordion">
       {accordionItemsData.map((item, index) => (
-        <AccordionItem key={index} data={item} context={context} />
+        <AccordionItem
+          key={index}
+          data={item}
+          context={context}
+          onToggle={() => handleToggle(index)}
+          active={clicked === index}
+        />
       ))}
     </ul>
   );
--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import MechClipsCollection from "../MechClipsCollection/MechClipsCollection";
 import "./AccordionItem.scss";
 
-const AccordionItem = ({ data, context }) => {
+const AccordionItem = ({ data, context, onToggle, active }) => {
   return (
     <li className="accordion-item">
-      <button className="accordion-item__header">
+      <button className="accordion-item__header" onClick={onToggle}>
         {data.accordionItemTitle}
-        <i className="fa-solid fa-caret-down"></i>
+        {active ? (
+          <i className="fa-solid fa-caret-down accordion-item__control accordion-item__control--active"></i>
+        ) : (
+          <i className="fa-solid fa-caret-down accordion-item__control"></i>
+        )}
       </button>
       {context === "clips" ? <MechClipsCollection mech={data} /> : "Dummy item"}
     </li>
