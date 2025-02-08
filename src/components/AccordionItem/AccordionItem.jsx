b8ef892e135cae65663b1c29d858a7be9c1ebbb3
jacquikeating <j.keating@live.ca>
2025-02-08 16:11:11 -0500

    Cleaned up commented code

--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -14,33 +14,10 @@ const AccordionItem = ({ data, context, onToggle, active }) => {
             <i className="fa-solid fa-caret-down accordion-item__control"></i>
           )}
         </button>
-        <MechClipsCollection
-          //   className={`${
-          //     active
-          //       ? "accordion-item__content"
-          //       : "accordion-item__content accordion-item__content--open"
-          //   }`}
-          //   className="accordion-item__content"
-          mech={data}
-          active={active}
-        />
+        <MechClipsCollection mech={data} active={active} />
       </li>
     );
   }
-
-  //   return (
-  //     <li className="accordion-item">
-  //       <button className="accordion-item__header" onClick={onToggle}>
-  //         {data.accordionItemTitle}
-  //         {active ? (
-  //           <i className="fa-solid fa-caret-down accordion-item__control accordion-item__control--active"></i>
-  //         ) : (
-  //           <i className="fa-solid fa-caret-down accordion-item__control"></i>
-  //         )}
-  //       </button>
-  //       {context === "clips" ? <MechClipsCollection mech={data} /> : "Dummy item"}
-  //     </li>
-  //   );
 };
 
 export default AccordionItem;
