2559f5106ba918258a9e34d3b46390e370998407
jacquikeating <j.keating@live.ca>
2025-02-27 01:04:01 -0500

    Fixed typo
    - Switched "Crystallize Time" for "Crystalize Time" (one L) to match entered data.

--- src/pages/MechsPage/MechsPage.jsx
+++ src/pages/MechsPage/MechsPage.jsx
@@ -12,7 +12,7 @@ const MechsPage = () => {
     ["Opener", "Utopian Sky", "Fall of Faith", "Towers", "P1 Enrage"],
     ["Diamond Dust", "Mirrors", "Light Rampant", "P2 Enrage", "Intermission"],
     ["Ultimate Relativity", "Apocalypse", "P3 Enrage"],
-    ["Darklit Dragonsong", "Crystallize Time", "P4 Enrage"],
+    ["Darklit Dragonsong", "Crystalize Time", "P4 Enrage"],
     [
       "Fulgent Blade",
       "Wings Dark and Light",
