b67b963895a8910d9738de4883600043022626e6
jacquikeating <j.keating@live.ca>
2025-04-04 22:48:39 -0400

    Copied timeline <table> element
    - Copied code for timeline <table> from TimelinePage into Timeline component.
    - Commented out code that relies on unavailable arrays.
    - Added in phases as an empty array, to act as a placeholder for a prop passed down to the component.

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -2,7 +2,45 @@ import React from "react";
 import "./Timeline.scss";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  const phases = [];
+
+  return (
+    <div className="timeline">
+      <table className="timeline__table">
+        <tbody>
+          {phases.map((phase) => {
+            return (
+              <>
+                <tr className="timeline__header-row">
+                  <th className="timeline__header-cell timeline__header-cell--phase">
+                    {/* <h2 className="timeline__subheading">{phase.phaseName}</h2> */}
+                  </th>
+                  <th className="timeline__header-cell timeline__header-cell--name"></th>
+                  <th className="timeline__header-cell timeline__header-cell--nick"></th>
+                </tr>
+
+                {/* {filterMechs(phase.phaseNum).map((mech) => {
+                  return (
+                    <tr className="timeline__row">
+                      <td className="timeline__cell timeline__cell--time">
+                        {mech.time}
+                      </td>
+                      <td className="timeline__cell timeline__cell--name">
+                        {mech.name}
+                      </td>
+                      <td className="timeline__cell timeline__cell--nick">
+                        {mech.nickname}
+                      </td>
+                    </tr>
+                  );
+                })} */}
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Timeline;
