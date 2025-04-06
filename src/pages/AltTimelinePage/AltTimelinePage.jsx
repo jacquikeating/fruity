5d6eee62be08641f1cc8ce126d8a9809fecdd72f
jacquikeating <j.keating@live.ca>
2025-04-05 23:20:04 -0400

    Imported Timeline into AltTimelinePage
    - Imported Timeline component into AltTimelinePage.

--- src/pages/AltTimelinePage/AltTimelinePage.jsx
+++ src/pages/AltTimelinePage/AltTimelinePage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Timeline from "../../components/Timeline/Timeline.jsx";
 import "./AltTimelinePage.scss";
 
 const AltTimelinePage = () => {
-  return <div>AltTimelinePage</div>;
+  return <Timeline />;
 };
 
 export default AltTimelinePage;
