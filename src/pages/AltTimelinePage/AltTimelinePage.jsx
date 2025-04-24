8932ce696176fcc4695e786d381f6018c8207e47
jacquikeating <j.keating@live.ca>
2025-04-23 20:38:35 -0400

    Created separate file for fight timelines
    - Created a new file, fight-timelines.js, in the utils folder.
    - For organizational purposes, all fight timeline data will be stored here rather than at the page or component level.
    - Imported m5sTimeline into AltTimelinePage and replaced all existing references with the new one.

--- src/pages/AltTimelinePage/AltTimelinePage.jsx
+++ src/pages/AltTimelinePage/AltTimelinePage.jsx
@@ -1,63 +1,12 @@
 import React from "react";
 import Timeline from "../../components/Timeline/Timeline.jsx";
+import { m5sTimeline } from "../../utils/fight-timelines.js";
 import "./AltTimelinePage.scss";
 
 const AltTimelinePage = () => {
-  let fightTimeline = {
-    fightName: "Dancing Green",
-    phases: [
-      {
-        phaseName: "",
-        phaseNum: 1,
-        mechanics: [
-          {
-            name: "Deep Cut",
-            time: "0:14",
-            damage: 309,
-            desc: "double tankbuster",
-            pre: ["ED tanks", "Spire", "Exalt"],
-            post: ["Tauro"],
-          },
-          {
-            name: "Play A/B Side",
-            time: "0:38",
-            damage: 132,
-            desc: "roles/parties",
-            pre: [],
-            post: ["Oppo", "Phys"],
-          },
-          {
-            name: "Play A/B Side",
-            time: "0:58",
-            damage: 132,
-            desc: "roles/parties",
-            pre: ["Pan", "Kera", "Macro"],
-            post: ["Star", "Lady", "Ixo"],
-          },
-          {
-            name: "Celebrate Good Times",
-            time: "1:04",
-            damage: 181,
-            desc: "raidwide",
-            pre: [],
-            post: ["Micro", "Horo"],
-          },
-          {
-            name: "Disco Infernal",
-            time: "1:7",
-            damage: 132,
-            desc: "raidwide",
-            pre: [],
-            post: ["Micro", "Horo"],
-          },
-        ],
-      },
-    ],
-  };
-
   return (
     <main>
-      <Timeline fightTimeline={fightTimeline} />
+      <Timeline fightTimeline={m5sTimeline} />
     </main>
   );
 };
---
+++ src/utils/fight-timelines.js
@@ -0,0 +1,51 @@
+export const m5sTimeline = {
+  fightName: "Dancing Green",
+  phases: [
+    {
+      phaseName: "",
+      phaseNum: 1,
+      mechanics: [
+        {
+          name: "Deep Cut",
+          time: "0:14",
+          damage: 309,
+          desc: "double tankbuster",
+          pre: ["ED tanks", "Spire", "Exalt"],
+          post: ["Tauro"],
+        },
+        {
+          name: "Play A/B Side",
+          time: "0:38",
+          damage: 132,
+          desc: "roles/parties",
+          pre: [],
+          post: ["Oppo", "Phys"],
+        },
+        {
+          name: "Play A/B Side",
+          time: "0:58",
+          damage: 132,
+          desc: "roles/parties",
+          pre: ["Pan", "Kera", "Macro"],
+          post: ["Star", "Lady", "Ixo"],
+        },
+        {
+          name: "Celebrate Good Times",
+          time: "1:04",
+          damage: 181,
+          desc: "raidwide",
+          pre: [],
+          post: ["Micro", "Horo"],
+        },
+        {
+          name: "Disco Infernal",
+          time: "1:7",
+          damage: 132,
+          desc: "raidwide",
+          pre: [],
+          post: ["Micro", "Horo"],
+        },
+      ],
+    },
+  ],
+};
