b57b1b55921c650dfc6832f77de195be029b3403
jacquikeating <j.keating@live.ca>
2025-05-23 20:07:57 -0800

    Began implementing Accordion into AltTimelinePage
    - Imported Accordion component into AltTimelinePage.
    - Created accordionitemsData variable. It is an array, though it currently has only one item.
    - Added <Accordion> element with appropriate props.

--- src/pages/AltTimelinePage/AltTimelinePage.jsx
+++ src/pages/AltTimelinePage/AltTimelinePage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Accordion from "../../components/Accordion/Accordion";
 import Timeline from "../../components/Timeline/Timeline.jsx";
 import { m5sTimeline } from "../../utils/fight-timelines.js";
 import "./AltTimelinePage.scss";
 
 const AltTimelinePage = () => {
+  const accordionItemsData = [
+    {
+      accordionItemTitle: "(M5S) Dancing Green",
+      content: m5sTimeline,
+    },
+  ];
+
   return (
     <main>
       <h1>Timelines</h1>
+      <Accordion
+        accordionItemsData={accordionItemsData}
+        context={"timelines"}
+      />
+
       <Timeline fightTimeline={m5sTimeline} />
     </main>
   );
