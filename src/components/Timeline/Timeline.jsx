492d4cf7c14f839b15d1491333ac6da3fdd97c69
jacquikeating <j.keating@live.ca>
2025-06-09 23:01:46 -0500

    Removed phases from Timeline components
    - Adjusted JSX code in the Timeline component so that the tables of mechanics can be rendered, now that they are un-nested from the phases arrays.

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -29,82 +29,64 @@ const Timeline = ({ fightTimeline, active }) => {
       />
       <table className="tl__table">
         <tbody>
-          {fightTimeline.phases.map((phase) => {
+          <tr className="tl__header-row">
+            <th className="tl__header-cell tl__header-cell--time">Time</th>
+            <th className="tl__header-cell tl__header-cell--name">Name</th>
+            <th className="tl__header-cell tl__header-cell--dmg">Dmg</th>
+            <th className="tl__header-cell tl__header-cell--desc">
+              Description
+            </th>
+            <th className="tl__header-cell tl__header-cell--mits-heals">
+              Mits & Heals
+            </th>
+          </tr>
+
+          {fightTimeline.mechanics.map((mech) => {
             return (
-              <>
-                {phase.phaseName ? (
-                  <h3 className="tl__phase-name">{phase.phaseName}</h3>
-                ) : (
-                  ""
-                )}
+              <tr className="tl__row">
+                <td className="tl__cell tl__cell--time">{mech.time}</td>
+                <td className="tl__cell tl__cell--name">{mech.name}</td>
+                <td className="tl__cell tl__cell--damage">{mech.damage}k</td>
+                <td className="tl__cell tl__cell--desc">{mech.desc}</td>
+                <td className="tl__cell tl__cell--mits-heals">
+                  {mech.pre.map((action, index) => {
+                    if (displayIcons && action.icon) {
+                      return (
+                        <img
+                          src={action.icon}
+                          alt={action.name}
+                          className="tl__action-icon"
+                        />
+                      );
+                    } else if (index != 0) {
+                      return <span>, {action.abbr}</span>;
+                    } else {
+                      return <span>{action.abbr}</span>;
+                    }
+                  })}
+                  {mech.pre.length > 0 && mech.post.length > 0 ? (
+                    <span className="tl__arrow"> → </span>
+                  ) : (
+                    ""
+                  )}
 
-                <tr className="tl__header-row">
-                  <th className="tl__header-cell tl__header-cell--time">
-                    Time
-                  </th>
-                  <th className="tl__header-cell tl__header-cell--name">
-                    Name
-                  </th>
-                  <th className="tl__header-cell tl__header-cell--dmg">Dmg</th>
-                  <th className="tl__header-cell tl__header-cell--desc">
-                    Description
-                  </th>
-                  <th className="tl__header-cell tl__header-cell--mits-heals">
-                    Mits & Heals
-                  </th>
-                </tr>
-
-                {phase.mechanics.map((mech) => {
-                  return (
-                    <tr className="tl__row">
-                      <td className="tl__cell tl__cell--time">{mech.time}</td>
-                      <td className="tl__cell tl__cell--name">{mech.name}</td>
-                      <td className="tl__cell tl__cell--damage">
-                        {mech.damage}k
-                      </td>
-                      <td className="tl__cell tl__cell--desc">{mech.desc}</td>
-                      <td className="tl__cell tl__cell--mits-heals">
-                        {mech.pre.map((action, index) => {
-                          if (displayIcons && action.icon) {
-                            return (
-                              <img
-                                src={action.icon}
-                                alt={action.name}
-                                className="tl__action-icon"
-                              />
-                            );
-                          } else if (index != 0) {
-                            return <span>, {action.abbr}</span>;
-                          } else {
-                            return <span>{action.abbr}</span>;
-                          }
-                        })}
-                        {mech.pre.length > 0 && mech.post.length > 0 ? (
-                          <span className="tl__arrow"> → </span>
-                        ) : (
-                          ""
-                        )}
-
-                        {mech.post.map((action, index) => {
-                          if (displayIcons && action.icon) {
-                            return (
-                              <img
-                                src={action.icon}
-                                alt={action.name}
-                                className="tl__action-icon"
-                              />
-                            );
-                          } else if (index != 0) {
-                            return <span>, {action.abbr}</span>;
-                          } else {
-                            return <span>{action.abbr}</span>;
-                          }
-                        })}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </>
+                  {mech.post.map((action, index) => {
+                    if (displayIcons && action.icon) {
+                      return (
+                        <img
+                          src={action.icon}
+                          alt={action.name}
+                          className="tl__action-icon"
+                        />
+                      );
+                    } else if (index != 0) {
+                      return <span>, {action.abbr}</span>;
+                    } else {
+                      return <span>{action.abbr}</span>;
+                    }
+                  })}
+                </td>
+              </tr>
             );
           })}
         </tbody>
