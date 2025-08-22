092eb19186775f7653319309fe3231edd3a76cd4
jacquikeating <j.keating@live.ca>
2025-08-21 21:38:41 -0400

    Fully implemented TanStack Query!
    - Moved QueryClient and QueryClientProvider into main.jsx.
    - Wrapped <QueryClientProvider> around <App>.
    - Created getSessions() funciton and a useQuery() call in App.
    - Deleted all code from old fetching functions, including sessionsState and imports.
    - Replaced all references to sessionsState with data from useQuery().
    - Updated the "rip" text to "Loading..."

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useAxiosGet, useAxios } from "./hooks/useFetch";
+import { useQuery } from "@tanstack/react-query";
 import Header from "./components/Header/Header";
 import AddDataPage from "./pages/AddDataPage/AddDataPage";
 import InfoPage from "./pages/InfoPage/InfoPage";
@@ -13,90 +12,45 @@ import ReportPage from "./pages/ReportPage/ReportPage";
 import TimelinePage from "./pages/TimelinePage/TimelinePage";
 import AltTimelinePage from "./pages/AltTimelinePage/AltTimelinePage";
 import "./styles/index.scss";
-
-import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
-  const { data: sessions, error, loading } = useAxiosGet(`sessions`);
-  useEffect(() => {
-    if (sessions.length > 0) {
-      setSessionsState(sessions);
-    }
-  }, [sessions]);
+  const getSessions = async () => {
+    const response = await fetch(`${API_URL}/sessions`);
+    return response.json();
+  };
 
-  const [sessionsState, setSessionsState] = useState(null);
-
-  // const { response } = useAxios(
-  //   {
-  //     method: "get",
-  //     url: "/sessions",
-  //   },
-  //   true
-  // );
-
-  // useEffect(() => {
-  //   if (response !== null) {
-  //     // console.log(response);
-  //     setSessionsState(response);
-  //   }
-  // }, [response]);
-
-  function Test({ setSessionsState }) {
-    const API_URL = import.meta.env.VITE_API_URL;
-
-    const { isPending, error, data, isFetching } = useQuery({
-      queryKey: ["sessionsData"],
-      queryFn: async () => {
-        const response = await fetch(`${API_URL}/sessions`);
-        const testData = await response.json();
-        console.log(testData);
-        setSessionsState(response.data);
-        return testData;
-      },
-    });
-
-    if (isPending) return "Loading...";
-
-    if (error) return "An error has occurred: " + error.message;
-  }
+  const { data, isPending } = useQuery({
+    queryKey: ["sessions"],
+    queryFn: getSessions,
+  });
 
   return (
     <>
-      {sessionsState ? (
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            {/* <Test setSessionsState={setSessionsState} /> */}
-            <Header latestSession={sessionsState.length} />
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <OverviewPage sessions={[...sessionsState].reverse()} />
-                }
-              />
-              <Route
-                path="/report/:sessionID"
-                element={<ReportPage sessions={sessionsState} />}
-              />
-              <Route path="/add-data" element={<AddDataPage />} />
-              <Route path="/about" element={<InfoPage />} />
-              <Route path="/prog" element={<MechsPage />} />
-              <Route path="/clips" element={<NotesPage />} />
-              <Route path="/timeline" element={<TimelinePage />} />
-              <Route path="/alt-timeline" element={<AltTimelinePage />} />
-              <Route path="/account" element={<LoginPage />} />
-              <Route path="/*" element={<NotFoundPage />} />
-            </Routes>
-          </BrowserRouter>
-        </QueryClientProvider>
+      {!isPending ? (
+        <BrowserRouter>
+          <Header latestSession={data.length} />
+          <Routes>
+            <Route
+              path="/"
+              element={<OverviewPage sessions={[...data].reverse()} />}
+            />
+            <Route
+              path="/report/:sessionID"
+              element={<ReportPage sessions={data} />}
+            />
+            <Route path="/add-data" element={<AddDataPage />} />
+            <Route path="/about" element={<InfoPage />} />
+            <Route path="/prog" element={<MechsPage />} />
+            <Route path="/clips" element={<NotesPage />} />
+            <Route path="/timeline" element={<TimelinePage />} />
+            <Route path="/alt-timeline" element={<AltTimelinePage />} />
+            <Route path="/account" element={<LoginPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
       ) : (
-        <p>rip</p>
+        <p>Loading...</p>
       )}
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./styles/index.scss";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       clientId="4wqqQ23Y9I6zh4XK2UDQon4YWvFS9Oxs"
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Auth0Provider>
   </React.StrictMode>
 );
