e5c38c2b2ae0407dd8d012823b233d12539269b6
jacquikeating <j.keating@live.ca>
2025-08-22 23:40:13 -0400

    Upgraded useGetSessions() to use Axios
    - Created an axiosInstance in use-get-sessions.js.
    - Replaced the fetch() function with axios.get().

--- src/hooks/use-get-sessions.js
+++ src/hooks/use-get-sessions.js
@@ -1,11 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
 export default function useGetSessions() {
   const getSessions = async () => {
-    const response = await fetch(`${API_URL}/sessions`);
-    return response.json();
+    try {
+      const response = await axiosInstance.get("/sessions");
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   };
 
   const { data, isPending } = useQuery({
