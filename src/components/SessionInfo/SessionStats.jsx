1f12e15cb7a7d088241e7e12c408dd683395d3c5
jacquikeating <j.keating@live.ca>
2025-08-24 15:51:31 -0400

    Imported functions to SessionStats and added contetn
    - Copied content from SessionInfo/SessionInfoEdit into SessionStats.
    - Imported required functions and context.

--- src/components/SessionInfo/SessionStats.jsx
+++ src/components/SessionInfo/SessionStats.jsx
@@ -1,5 +1,36 @@
+import {
+  findGoldStars,
+  findStrugglePhase,
+  findStruggleMech,
+} from "../../utils/shared-functions.js";
+import { useContext } from "react";
+import { SessionContext } from "../../pages/ReportPage/ReportPage.jsx";
+
 const SessionStats = () => {
-  return <p>stats</p>;
+  const { sessionCtx } = useContext(SessionContext);
+  const { session, pullsArray, sessionID } = sessionCtx;
+  return (
+    <>
+      <p className="report__extra-info">
+        <span className="report__extra-info--bold">Most Wipes: </span>
+        Phase {findStrugglePhase(pullsArray)}
+        <span className="report__divider"> • </span>
+        {findStruggleMech(pullsArray)}
+      </p>
+      <p className="report__extra-info">
+        <span className="report__extra-info--bold">Gold Stars: </span>
+        {findGoldStars(pullsArray, session.roster)}
+      </p>
+      {sessionID == 14 ? (
+        <p className="report__extra-info">
+          <span className="report__extra-info--bold">💩 Star: </span>
+          Sophia
+        </p>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
 
 export default SessionStats;
