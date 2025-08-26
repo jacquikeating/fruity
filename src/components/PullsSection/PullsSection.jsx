6cfcf4ec6238ffb736f4dddd41735177c5aa7e7c
jacquikeating <j.keating@live.ca>
2025-08-26 17:50:16 -0400

    Imported context into PullsSection
    - Imported all 3 contexts. For now I have included a long list of variables, but this will eventually be whittled down to the bare minimum.
    - Destructured variables.

--- src/components/PullsSection/PullsSection.jsx
+++ src/components/PullsSection/PullsSection.jsx
@@ -1,4 +1,34 @@
+import { useState, useContext } from "react";
+import {
+  SessionContext,
+  PullsContext,
+  EditContext,
+} from "../../pages/ReportPage/ReportPage.jsx";
+
 const PullsSection = () => {
+  const { sessionCtx } = useContext(SessionContext);
+  const { session, pullsArray, sessionID } = sessionCtx;
+  const { editCtx } = useContext(EditContext);
+  const {
+    editMode,
+    setEditMode,
+    showEdit,
+    allowDelete,
+    updatePull,
+    deletePull,
+    pullToUpdate,
+  } = editCtx;
+  const { pullsCtx } = useContext(PullsContext);
+  const {
+    width,
+    breakpoint,
+    setPullsArray,
+    pullsToDisplay,
+    setPullsToDisplay,
+    getProgPulls,
+    filterPulls,
+    handleCheckbox,
+  } = pullsCtx;
   return <p>PullsSection testing</p>;
 };
 
