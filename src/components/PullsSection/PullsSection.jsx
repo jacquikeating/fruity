bc2f4fef701560be98435c3bc79f3e677e5d54fb
jacquikeating <j.keating@live.ca>
2025-08-26 18:16:45 -0400

    Added progOnlyCheckbox to PullsSection
     - Copied in JSX for progOnlyCheckbox into PullsSection.
    - Copied in several of the states and functions related to this functionality, and commented out the duplicates coming from contexts. All of this stuff will be fully moved to PullsSection later.

--- src/components/PullsSection/PullsSection.jsx
+++ src/components/PullsSection/PullsSection.jsx
@@ -23,17 +23,49 @@ const PullsSection = () => {
     width,
     breakpoint,
     setPullsArray,
-    pullsToDisplay,
-    setPullsToDisplay,
-    getProgPulls,
-    filterPulls,
-    handleCheckbox,
+    // pullsToDisplay,
+    // setPullsToDisplay,
+    // getProgPulls,
+    // filterPulls,
+    // handleCheckbox,
   } = pullsCtx;
+
+  const [progPullsOnly, setProgPullsOnly] = useState(false);
+  const [pullsToDisplay, setPullsToDisplay] = useState([]);
+
+  function handleCheckbox() {
+    if (progPullsOnly) {
+      setProgPullsOnly(false);
+    } else {
+      setProgPullsOnly(true);
+    }
+  }
+
+  function getProgPulls() {
+    const filteredPullsArray = pullsToDisplay.filter(
+      (pull) =>
+        pull.mech === session.prog_mech ||
+        pull.mech === getMechAfterProgMech(session.prog_mech)
+    );
+    return filteredPullsArray;
+  }
 
   return (
     <section className="report__section">
       <div className="report__pulls-heading">
         <h2 className="report__subheading">Pulls ({pullsArray.length})</h2>
+
+        <label className="report__filter-label" htmlFor="progOnlyCheckbox">
+          <input
+            type="checkbox"
+            name="progOnlyCheckbox"
+            id="progOnlyCheckbox"
+            className="report__filter-input"
+            value={progPullsOnly}
+            onChange={handleCheckbox}
+          />
+          Show prog pulls only
+        </label>
       </div>
     </section>
   );
