c23748c310542e5ebd3fc8d871713c64e9e730bf
jacquikeating <j.keating@live.ca>
2025-08-27 19:32:48 -0400

    Created pullsStore
    - Created another store file in /stores, pullStore.js.
    - Did basic setup.
    - Copied in some of the props that are currently being drilled the hell down, as a "to do list" for this store.

---
+++ src/stores/pullsStore.js
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+export const pullsStore = create((set) => ({
+  /* 
+    pullData,
+    pullNumType,
+    showEdit,
+    updatePull,
+    deletePull,
+    progPhase,
+    allowDelete,
+  */
+}));
